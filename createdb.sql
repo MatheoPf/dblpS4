b629f929fb2684fcb0e57e2ff26e6560711eb3f3
MatheoPf <matheo.pfranger@gmail.com>
2025-03-03 08:59:19 +0100

    maj

--- createdb.sql
+++ createdb.sql
@@ -1,27 +1,23 @@
-create schema AnalyseGeo;
+-- create schema AnalyseGeo;
 set schema 'AnalyseGeo';
 
-CREATE TABLE AnalyseGeo.import_villes (
-    city VARCHAR(50),
-    city_ascii VARCHAR(50),
-    lat FLOAT,
-    lng FLOAT,
-    country VARCHAR(50),
-    iso2 CHAR(2),
-    iso3 CHAR(3),
-    admin_name VARCHAR(255),
-    capital VARCHAR(50),
+create table if not exists AnalyseGeo.import_villes (
+    city varchar(50),
+    city_ascii varchar(50),
+    lat float,
+    lng float,
+    country varchar(50),
+    iso2 char(2),
+    iso3 char(3),
+    admin_name varchar(255),
+    capital varchar(50),
     population varchar(255),
-    id INTEGER PRIMARY KEY
+    id integer primary key
 );
-
-copy AnalyseGeo.import_villes
-from 'D:\iut\Semestre2\Analyse\worldcities.csv'
-with (FORMAT csv, header true, delimiter ',');
+-- drop table AnalyseGeo.import_villes;
 
 -- Création de la table _villes
---drop table AnalyseGeo._villes;
-create table AnalyseGeo._villes (
+create table if not exists AnalyseGeo._villes (
 	id integer not null primary key,
 	nom_ville varchar(50) not null,
 	latitude float not null,
@@ -29,6 +25,18 @@ create table AnalyseGeo._villes (
 	iso char(4) not null,
 	nom_pays varchar(50) not null
 );
+--drop table AnalyseGeo._villes;
+
+create table if not exists AnalyseGeo.pays_continent(
+  continent varchar(15),
+  pays varchar(50)
+  );
+-- drop table AnalyseGeo.pays_continent;
+
+--	copy
+copy AnalyseGeo.import_villes
+from 'D:\iut\Semestre2\Analyse\worldcities.csv'
+with (format csv, header true, delimiter ',');
 
 -- Insertion de import_villes à villes
 insert into AnalyseGeo._villes (id, nom_ville, latitude, longitude, iso, nom_pays)
@@ -36,17 +44,13 @@ select distinct id, city_ascii, lat, lng, iso3, country
 from AnalyseGeo.import_villes
 on conflict (id) do nothing;
 
+-- copy
+copy AnalyseGeo.pays_continent (continent, pays)
+    from 'D:\iut\Semestre2\Analyse\Countries-Continents.csv' with delimiter ',' csv header; 
+
 -- Supprime la table temporaire
 drop table AnalyseGeo.import_villes;
 
--- drop table AnalyseGeo.pays_continent;
-create table if not exists AnalyseGeo.pays_continent(
-  continent varchar(15),
-  pays varchar(50)
-  );
-  
-copy AnalyseGeo.pays_continent (continent, pays)
-    from 'D:\iut\Semestre2\Analyse\Countries-Continents.csv' WITH DELIMITER ',' CSV HEADER; 
-
+ 
 create or replace view AnalyseGeo.touteslesinfo as (
-  select * from AnalyseGeo._villes inner join AnalyseGeo.pays_continent on pays = nom_ville);
+  select * from AnalyseGeo._villes inner join AnalyseGeo.pays_continent on pays = nom_pays);
