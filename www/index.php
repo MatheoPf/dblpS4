b350c285a4260a48ef5e3e6857bf65027c220a69
MatheoPf <matheo.pfranger@gmail.com>
2025-03-18 17:00:27 +0100

    auto createdb

--- www/index.php
+++ www/index.php
@@ -1,1 +1,13 @@
-<?php phpinfo();?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Titre</h1>
+    <a href="remplir.php">Créer et Remplir la base de données</a>
+    <a href="">Exploiter l'outil</a>
+</body>
+</html>
