fdaf278c12ab01ccc5ead0a9315e09d5b2f61640
capoupon <camille.poupon@etudiant.univ-rennes.fr>
2025-03-19 16:46:37 +0100

    ajout de 8000 pages tests

---
+++ www/testaffichage.php
@@ -0,0 +1,235 @@
+<?php
+
+$server = 'db';
+$dbname = 'mydatabase';
+$user = 'user';
+$pass = 'password';
+
+
+if (isset($_GET['nom'])) {
+    $nom_auteur = urldecode($_GET['nom']); // Décodage pour afficher correctement
+    echo "<h1>Détails de l'auteur : $nom_auteur</h1>";
+    // Tu peux maintenant rechercher l'auteur dans la base et afficher plus d'infos
+} else {
+    echo "<h1>Aucun auteur sélectionné.</h1>";
+}
+
+ $req_get_auteur = "Select pid from analysegeo._auteurs where nom = '.$nom_auteur.'"; //on recupere le pid de l'auteur
+ $pid ;
+$req_get_publi_avec_auteur = "SELECT * from analysegeo.a_ecrit where pid =" .$pid. ";"; //on recupere tous les publications (id_dblp) que l'auteur a ecrit
+$id_dblp;
+$req_revues_avec_pid = "SELECT * FROM analysegeo._revues where id_dblp = id_dblp"; //on recupere les infos
+ $req_conferences_avec_pid = "SELECT * FROM analysegeo._conferences where id_dblp = id_dblp";
+    
+ try {
+     $conn = new PDO("pgsql:host=$server;dbname=$dbname", $user, $pass);
+     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Ajout pour gérer les erreurs proprement
+     $conn->prepare("SET SCHEMA 'analysegeo';")->execute();
+     
+     // Exécuter les requêtes
+     $stmt_get_auteur = $conn->prepare($req_get_auteur);
+     $stmt_revues->execute();
+     $pid_lauteur = $stmt_get_auteur->fetchAll(PDO::FETCH_ASSOC);
+
+     $stmt_get_publi_avec_auteur = $conn->prepare($req_get_publi_avec_auteur);
+     $stmt_get_publi_avec_auteur->execute();
+     $id_dblp = $stmt_revues->fetchAll(PDO::FETCH_ASSOC);
+
+
+    foreach ($id_dblp as $id) {
+        $stmt_revues = $conn->prepare($req_revues_avec_pid);
+        $stmt_revues->execute();
+        $revues = array_merge($revues, $stmt_revues->fetchAll(PDO::FETCH_ASSOC));
+    }
+
+    foreach ($id_dblp as $id) {
+        $stmt_conference = $conn->prepare($req_conferences_avec_pid);
+        $stmt_conference->execute();
+        $revues = array_merge($conference, $stmt_conference->fetchAll(PDO::FETCH_ASSOC));
+    }
+  
+     $conn = null; 
+
+     
+
+    }catch(PDOExeception $e){
+        print "Erreur PDO : " . $e->getMessage() . "<br/>";
+    }
+
+    
+
+    // Fonction pour obtenir les noms des auteurs d'une publication
+    function getAuteursForPublication($id_dblp, $a_ecrit, $auteurs) {
+        $noms_auteurs = [];
+
+        foreach ($a_ecrit as $relation) {
+            if ($relation['id_dblp'] == $id_dblp) {
+                foreach ($auteurs as $auteur) {
+                    if ($auteur['pid'] == $relation['pid']) {
+                        // Encodage du nom pour éviter les problèmes d'URL
+                        $nom_enc = urlencode($auteur['nom']);
+                        // Création d'un lien cliquable vers une page auteur.php
+                        $noms_auteurs[] = "<a href='auteur.php?nom={$nom_enc}'>{$auteur['nom']}</a>";
+                    }
+                }
+            }
+        }
+
+        return implode(", ", $noms_auteurs); // Convertit en chaîne séparée par des virgules
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+     integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obThR9BMY="
+     crossorigin=""/>
+
+     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+     integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+     crossorigin=""></script>
+    <title>Document</title>
+    </head>
+    <body>
+
+    <table border="1">
+        <thead>
+            <tr>
+                <th>
+                    ID DLP
+                </th>
+                <th>
+                    Type
+                </th>
+                <th>
+                    DOI
+                </th>
+                <th>
+                    Titre
+                </th>
+                <th>
+                    Lieu
+                </th>
+                <th>
+                    Année
+                </th>
+                <th>
+                    Pages
+                </th>
+                <th>
+                    EE
+                </th>
+                <th>
+                    URL DBLP
+                </th>
+                <th>
+                    Volume
+                </th>
+                <th>
+                    Numero de Page
+                </th>
+                <th>
+                    Auteur
+                </th>
+            </tr>
+        </thead>
+        <tbody> 
+        <?php foreach($revues as $larevue){?>
+            <tr>
+                <td>
+                    <?php echo $larevue['id_dblp']?>
+                </td>
+                <td>
+                    <?php echo $larevue['type']?>
+                </td>
+                <td>
+                    <?php echo $larevue['doi']?>
+                </td>
+                <td>
+                    <?php echo $larevue['titre']?>
+                </td>
+                <td>
+                    <?php echo $larevue['lieu']?>
+                </td>
+                <td>
+                    <?php echo $larevue['annee']?>
+                </td>
+                <td>
+                    <?php echo $larevue['pages']?>
+                </td>
+                <td>
+                    <?php echo $larevue['ee']?>
+                </td>
+                <td>
+                    <?php echo $larevue['url_dblp']?>
+                </td>
+                <td>
+                    <?php echo $larevue['volume']?>
+                </td>
+                <td>
+                    <?php echo $larevue['numero']?>
+                </td>
+                <td>
+                    <?php echo getAuteursForPublication($larevue['id_dblp'], $a_ecrit, $auteurs);  ?>
+                </td>
+            <?php }
+                
+                ?>
+                
+            </tr>
+            <?php  foreach($conferences as $laconference){?>
+            <tr>
+                <td>
+                    <?php echo $laconference['id_dblp']?>
+                </td>
+                <td>
+                    <?php echo $laconference['type']?>
+                </td>
+                <td>
+                    <?php echo $laconference['doi']?>
+                </td>
+                <td>
+                    <?php echo $laconference['titre']?>
+                </td>
+                <td>
+                    <?php echo $laconference['lieu']?>
+                </td>
+                <td>
+                    <?php echo $laconference['annee']?>
+                </td>
+                <td>
+                    <?php echo $laconference['pages']?>
+                </td>
+                <td>
+                    <?php echo $laconference['ee']?>
+                </td>
+                <td>
+                    <?php echo $laconference['url_dblp']?>
+                </td>
+                <td>    
+                </td> <!-- Pas de volume pour les conférences -->
+                <td>
+                </td> <!-- Pas de numéro pour les conférences -->
+                <td>
+                <?php echo getAuteursForPublication($laconference['id_dblp'], $a_ecrit, $auteurs); ?>
+                    
+                </td>
+      
+            </tr>
+            <?php } ?>
+            
+
+        </tbody>
+
+
+        </table>
+    
+
+    
+</body>
+</html>
+
---
+++ www/testapipasludo.php
@@ -0,0 +1,186 @@
+<?php 
+
+$server = 'db';
+$dbname = 'mydatabase';
+$user = 'user';
+$pass = 'password';
+
+try {
+    $dbh = new PDO("pgsql:host=$server;dbname=$dbname", 
+            $user, $pass);
+    
+    $dbh->prepare("SET SCHEMA 'analysegeo';")->execute();
+    
+} catch (PDOException $e) {
+    print "Erreur !: " . $e->getMessage() . "<br/>";
+    die();
+}
+
+if (isset($_GET['nom'])) {
+    $nom_auteur = urldecode($_GET['nom']); // Décodage pour afficher correctement
+    echo "<h1>Détails de l'auteur : $nom_auteur</h1>";
+    // Tu peux maintenant rechercher l'auteur dans la base et afficher plus d'infos
+} else {
+    echo "<h1>Aucun auteur sélectionné.</h1>";
+}
+
+$nom_auteur_formatte = str_replace(' ', '_', $nom_auteur); // Remplace les espaces par _
+$lien = "https://dblp.org/search/publ/api?q=author%3A" . urlencode($nom_auteur_formatte) . "%3A&format=json";
+
+
+$publi = file_get_contents($lien);
+$publi_decode=json_decode($publi, true);
+echo '<pre>';
+print_r($publi_decode);
+echo '</pre>';
+
+foreach ($publi_decode['result']['hits']['hit'] as $publi) {
+    if ($publi['info']['type'] == "Journal Articles" ||$publi['info']['type'] == "Conference and Workshop Papers") {
+        
+        $id_dblp = $publi['@id'];
+        echo "ID DBLP : " . $id_dblp . "<br><br>";
+
+        if (isset($publi['info']['type'])) {
+            $type = $publi['info']['type'];
+            echo "Type : " . $type . "<br><br>";       
+        }
+        
+        if ($publi['info']['doi']) {
+            $doi = $publi['info']['doi']; 
+            echo "DOI : " . $doi . "<br><br>";
+        }else {
+            $doi = null;
+        }
+        
+        if(isset($publi['info']['title'])){
+            $titre = $publi['info']['title'];
+            echo "Titre : " . $titre . "<br><br>";
+        }else {
+            $titre = null;
+        }
+       
+
+        if (isset($publi['info']['venue'])) {
+            $lieu = $publi['info']['venue'];
+            echo "Lieu : " . $lieu . "<br><br>";
+        }else{
+            $lieu = null;
+        }
+        
+
+        if (isset($publi['info']['year'])) {
+            $annee = $publi['info']['year'];
+            echo "Année : " . $annee. "<br><br>";
+        }else {
+            $annee = null;
+        }
+        
+        if (isset($publi['info']['pages'])) {
+            $pages = $publi['info']['pages'];
+            echo "Pages : " . $pages . "<br><br>";
+        }else {
+            $pages = null;
+        }
+        
+        
+        if(isset($publi['info']['ee'])){
+            $ee = $publi['info']['ee'];
+            echo "EE : " . $ee . "<br><br>";
+        }else {
+            $ee = null;
+        }
+        
+        if(isset($publi['info']['url'])){
+            $url_dblp = $publi['info']['url'];
+            echo "URL DBLP : " . $url_dblp . "<br><br>";
+        }else {
+            $url_dblp = null;
+        }
+        
+        if ($publi['info']['type'] == "Journal Articles"){
+
+            if(isset($publi['info']['volume'])){
+                $volume = $publi['info']['volume'];
+                echo "Volume : " . $volume . "<br><br>";
+            }else{
+                $volume = null;
+            }
+            
+            if(isset($publi['info']['number'])){
+                $numero_page = $publi['info']['number'];
+                echo "Numero de page : " . $numero_page . "<br><br>";
+            }  else {
+                $numero_page = null;
+            } 
+        }
+        
+        try {
+            $query_pub = "INSERT INTO AnalyseGeo._publications(id_dblp, type, doi, titre, lieu, annee, pages, ee, url_dblp) 
+              VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+              ON CONFLICT (id_dblp) DO NOTHING;";
+            $stmt_pub = $dbh->prepare($query_pub);
+            $stmt_pub->execute([$id_dblp, $type, $doi, $titre, $lieu, $annee, $pages, $ee, $url_dblp]);
+
+            switch ($type) {
+                case 'Journal Articles':
+                    $query = "INSERT INTO AnalyseGeo._revues(id_dblp, type, doi, titre, lieu, annee, pages, ee, url_dblp, volume, numero) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                    $stmt_publi = $dbh->prepare($query);
+                    $stmt_publi->execute([$id_dblp, $type, $doi, $titre, $lieu, $annee, $pages, $ee, $url_dblp, $volume, $numero_page]);
+                    break;
+            
+                case 'Conference and Workshop Papers' :
+                    $query = "INSERT INTO AnalyseGeo._conferences(id_dblp, type, doi, titre, lieu, annee, pages, ee, url_dblp) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                    $stmt_publi = $dbh->prepare($query);
+                    $stmt_publi->execute([$id_dblp, $type, $doi, $titre, $lieu, $annee, $pages, $ee, $url_dblp]);
+                    break;
+                
+                default:
+                    echo "probleme switch revues ou conference";
+                    break;
+            }
+        } catch (Exception $e) {
+            $dbh->rollBack();
+            print "Erreur PDO : " . $e->getMessage() . "<br/>";
+        }
+
+
+        
+        $ordre_auteur = 0;
+        foreach ($publi['info']['authors']['author'] as $auteur) {
+            $ordre_auteur += 1;
+            $auteur_pid = $auteur['@pid'];
+            echo "Auteur PID : ". $auteur_pid . "<br>";
+            
+            $auteur_nom = $auteur["text"];
+            echo "Auteur Nom : ". $auteur_nom . "<br><br>";
+            
+
+            // $auteur_orc_id = $auteur['orcid'];
+            
+
+            try {
+                $query = "INSERT INTO AnalyseGeo._auteurs(pid, nom) VALUES (?,?) ON CONFLICT (pid) DO NOTHING";
+                $stmt_auteur = $dbh->prepare($query);
+                $stmt_auteur->execute([$auteur_pid, $auteur_nom]);
+            } catch (PDOException $e) {
+                print "Erreur PDO auteur: " . $e->getMessage() . "<br/>";
+            }
+
+            try {
+                $query_a_ecrit = "INSERT INTO AnalyseGeo.a_ecrit(pid, id_dblp, ordre) VALUES (?,?,?);";
+                $stmt_a_ecrit = $dbh->prepare($query_a_ecrit);
+                $stmt_a_ecrit->execute([$auteur_pid, $id_dblp, $ordre_auteur]);
+            } catch (PDOException $e) {
+                print "Erreur PDO a ecrit: " . $e->getMessage() . "<br/>";
+            }
+        }
+        echo "<br>";
+        
+    }
+      
+}
+
+
+$dbh = null;
+
+?>
